2f22886fa7277c30ab0bd2af1314adc1cf211080
shaunbaker111 <shaunbaker111@gmail.com>
2026-04-27 19:36:52 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+// firebase-config.js
+export const FIREBASE_BASE_URL =
+  "https://rio-tracker-default-rtdb.europe-west1.firebasedatabase.app";
+
+export const LOCATION_PATH = "/dog/location.json";
+export const HISTORY_PATH =
+  '/dog/history.json?orderBy="timestamp"&limitToLast=50';
